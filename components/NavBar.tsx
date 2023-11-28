35763ff3b37161ce8dd503c2fc68a5ce6cdb75d8
Eregion <wuxize1995@gmail.com>
2023-11-27 23:36:04 -0500

    add basic css for navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,10 +2,9 @@ import '../styles/navbar.css';
 
 export default function NavBar (){
     return (
-    <div className="container">
-      <h1 className="icon"></h1> {/*add night mode icon*/}
-
-      <nav>
+    <div className="container mx-auto m-0 bg-gray-800 font-sans font-semibold">
+      <h1 className="icon float-left p-2"></h1> {/*add night mode icon*/}
+      <nav className='float-right'>
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">Projects</a></li>
